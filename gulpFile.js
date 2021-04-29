eb55498601cb83b69b841b21b49c2fb063e6a878
ryfoa6 <duckduckgoose384@gmail.com>
2021-04-29 11:21:23 -0700

    adjusted lobby table font-sizing

--- gulpFile.js
+++ gulpFile.js
@@ -8,20 +8,11 @@ gulp.task('minify-css', () =>
 		.src('public/css/*.css')
 		.pipe(concat('styles.css'))
 		.pipe(minifyCSS({ compatibility: 'ie8' }))
-		.pipe(gulp.dest('public/css/'))
+		.pipe(gulp.dest('public/dist/css/'))
 )
 
 gulp.task('minify-js', () =>
-	gulp
-		.src('public/js/*.js')
-		.pipe(concat('script.js'))
-		.pipe(minifyJS())
-		.pipe(gulp.dest('public/js/'))
+	gulp.src('public/js/*.js').pipe(minifyJS()).pipe(gulp.dest('public/dist/js/'))
 )
 
-gulp.task('watch', function () {
-	gulp.watch('./public/css/*.css', gulp.series('minify-css'))
-	gulp.watch('./public/js/*.js', gulp.series('minify-js'))
-})
-
-gulp.task('default', gulp.series('minify-css', 'minify-js', 'watch'))
+gulp.task('default', gulp.series('minify-css', 'minify-js'))
