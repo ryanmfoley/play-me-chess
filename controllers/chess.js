d95b2842d45d42dc2b0a727ea6e01982b84a919c
ryfoa6 <duckduckgoose384@gmail.com>
2021-06-05 15:13:28 -0700

    Uncomment user authentication code

--- controllers/chess.js
+++ controllers/chess.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-// const isAuthenticated = (req, res, next) => {
-// 	if (req.session.currentUser) {
-// 		return next()
-// 	} else res.redirect('/users/login')
-// }
+const isAuthenticated = (req, res, next) => {
+	if (req.session.currentUser) {
+		return next()
+	} else res.redirect('/users/login')
+}
 
-// router.use(isAuthenticated)
+router.use(isAuthenticated)
 
-// router.get('/', isAuthenticated, (req, res) => {
-// 	const { username } = req.session.currentUser
-// 	res.cookie('username', username)
+router.get('/', isAuthenticated, (req, res) => {
+	const { username } = req.session.currentUser
+	res.cookie('username', username)
 
-// 	res.sendFile('/public/chess.html', { root: process.cwd() })
-// })
+	res.sendFile('/public/chess.html', { root: process.cwd() })
+})
 
 router.get('/', (req, res) =>
 	res.sendFile('/public/chess.html', { root: process.cwd() })
