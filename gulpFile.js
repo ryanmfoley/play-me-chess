f626eb0beaddb715a68861e00c5ec48883605b40
ryfoa6 <ryanmfoley84@gmail.com>
2022-01-11 18:07:49 -0800

    Add sass converter to gulpFile.js

--- gulpFile.js
+++ gulpFile.js
@@ -1,17 +1,19 @@
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const minifyCSS = require('gulp-clean-css')
+const sass = require('gulp-sass')(require('sass'))
 
 gulp.task('minify-css', () =>
 	gulp
-		.src('public/css/*.css')
+		.src('public/scss/*.scss')
+		.pipe(sass())
 		.pipe(concat('styles.css'))
 		.pipe(minifyCSS({ compatibility: 'ie8' }))
 		.pipe(gulp.dest('public/dist/css/'))
 )
 
 gulp.task('watch', function () {
-	gulp.watch('./public/css/*.css', gulp.series('minify-css'))
+	gulp.watch('./public/scss/*.scss', gulp.series('minify-css'))
 })
 
 gulp.task('default', gulp.series('minify-css', 'watch'))
